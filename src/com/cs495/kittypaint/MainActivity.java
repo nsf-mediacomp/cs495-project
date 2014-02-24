56f78705877f4e7b24a83716ecb6249e64f2cca1
katzen <ben.1plus1@gmail.com>
2014-02-23 18:23:50 -0600

    i maek pony
    pony should pony pony

--- src/com/cs495/kittypaint/MainActivity.java
+++ src/com/cs495/kittypaint/MainActivity.java
@@ -2,7 +2,9 @@ package com.cs495.kittypaint;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void becomePony(View view){
+    	Intent intent = new Intent(this, PonyActivity.class);
+    	startActivity(intent);
     }
     
 }
